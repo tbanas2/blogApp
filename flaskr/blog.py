be2d2ff97fe5076812b35d6436049150e62b3e37
Luke Banaszak <thomas.banaszak@gmail.com>
2022-10-06 16:32:10 -0500

    storing url in DB but still not working

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -57,7 +57,7 @@ def create():
 #m123
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, pic'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
